c21af0819036c2615898890892988d76f874bd54
yoonjyyoonjy-sudo <yoonjyyoonjy@gmail.com>
2026-05-20 19:21:35 +0900

    Fix sitemap loading speed

--- cctv-map-upload/app/sitemap.xml/route.ts
+++ cctv-map-upload/app/sitemap.xml/route.ts
@@ -1,22 +1,21 @@
 import { NextResponse } from "next/server";
 
-import { countCctvRows } from "../../lib/cctvData";
-
 export const runtime = "nodejs";
 export const revalidate = 86400;
 
 const SITE_URL = "https://cctv.idlun.com";
 const SITEMAP_SIZE = 50000;
+const CCTV_TOTAL_COUNT = 357891;
 
-export async function GET() {
-  const total = await countCctvRows();
-  const pageCount = Math.ceil(total / SITEMAP_SIZE);
+export function GET() {
+  const pageCount = Math.ceil(CCTV_TOTAL_COUNT / SITEMAP_SIZE);
   const sitemaps = Array.from({ length: pageCount }, (_, index) => {
     return `<sitemap><loc>${SITE_URL}/sitemaps/cctv/${index}.xml</loc></sitemap>`;
   }).join("");
+  const staticSitemap = `<sitemap><loc>${SITE_URL}/sitemaps/static</loc></sitemap>`;
   const regionSitemap = `<sitemap><loc>${SITE_URL}/sitemaps/region</loc></sitemap>`;
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemaps}${regionSitemap}</sitemapindex>`;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${staticSitemap}${sitemaps}${regionSitemap}</sitemapindex>`;
 
   return new NextResponse(xml, {
     headers: {
